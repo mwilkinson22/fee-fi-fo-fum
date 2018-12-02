25e0e03accccc59d8a7729f9c849e3bd4cf82873
Mark Wilkinson <markwilko22@gmail.com>
2018-12-02 14:41:28 +0000

    fixed PlayerStatsCollections.js

--- models/rugby/PlayerStatsCollection.js
+++ models/rugby/PlayerStatsCollection.js
@@ -4,12 +4,9 @@ const { Schema } = mongoose;
 const playerStatTypes = require("../../constants/playerStatTypes");
 
 const playerStatsCollectionSchema = new Schema(
-	//The first mapValues allows us to filter by category (scoring/attack/defence)
-	_.mapValues(playerStatTypes, stats => {
-		//The second mapValues pulls the stat's initials as a field, and sets it to be a number
-		return _.mapValues(stats, stat => {
-			return { type: Number, default: null };
-		});
+	//Adds all stat keys to schema as Numbers
+	_.mapValues(playerStatTypes, () => {
+		return Number;
 	})
 );
 
