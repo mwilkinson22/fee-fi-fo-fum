4b6101382e61f0bb5ea138e39e9089d723d2a668
Mark Wilkinson <markwilko22@gmail.com>
2018-12-02 14:35:25 +0000

    Upon further consideration, storing the stat types in the db is overkill. Instead, we store them in playerStatTypes.js, and iterate over the keys when creating PlayerStatsCollection schema

---
+++ constants/playerStatTypes.js
@@ -0,0 +1,180 @@
+module.exports = {
+	scoring: {
+		T: {
+			singular: "Try",
+			plural: "Tries",
+			storedInDatabase: true,
+			moreIsBetter: true,
+			type: "scoring"
+		},
+		CN: {
+			singular: "Conversion",
+			plural: "Conversions",
+			storedInDatabase: true,
+			moreIsBetter: true,
+			type: "scoring"
+		},
+		PK: {
+			singular: "Penalty Goal",
+			plural: "Penalty Goals",
+			storedInDatabase: true,
+			moreIsBetter: true,
+			type: "scoring"
+		},
+		DG: {
+			singular: "Drop Goal",
+			plural: "Drop Goals",
+			storedInDatabase: true,
+			moreIsBetter: true,
+			type: "scoring"
+		},
+		MG: {
+			singular: "Missed Goal",
+			plural: "Missed goals",
+			storedInDatabase: true,
+			moreIsBetter: false,
+			type: "scoring"
+		},
+		KS: {
+			singular: "Kicking Success",
+			plural: "Kicking Success",
+			unit: "%",
+			storedInDatabase: false,
+			moreIsBetter: true,
+			type: "scoring"
+		},
+		TA: {
+			singular: "Assist",
+			plural: "Assists",
+			storedInDatabase: true,
+			moreIsBetter: true,
+			type: "scoring"
+		}
+	},
+	attack: {
+		TB: {
+			singular: "Tackle Bust",
+			plural: "Tackle Busts",
+			storedInDatabase: true,
+			moreIsBetter: true,
+			type: "attack"
+		},
+		AT: {
+			singular: "Attacking Kick",
+			plural: "Attacking Kicks",
+			storedInDatabase: true,
+			moreIsBetter: true,
+			type: "attack"
+		},
+		C: {
+			singular: "Carry",
+			plural: "Carries",
+			storedInDatabase: true,
+			moreIsBetter: true,
+			type: "attack"
+		},
+		M: {
+			singular: "Metre",
+			plural: "Metres",
+			unit: "m",
+			storedInDatabase: true,
+			moreIsBetter: true,
+			type: "attack"
+		},
+		AG: {
+			singular: "Average Gain",
+			plural: "Average Gain",
+			unit: "m",
+			storedInDatabase: false,
+			moreIsBetter: true,
+			type: "attack"
+		},
+		CB: {
+			singular: "Clean Break",
+			plural: "Clean Breaks",
+			storedInDatabase: true,
+			moreIsBetter: true,
+			type: "attack"
+		},
+		DR: {
+			singular: "Dummy Run",
+			plural: "Dummy Runs",
+			storedInDatabase: true,
+			moreIsBetter: true,
+			type: "attack"
+		},
+		E: {
+			singular: "Error",
+			plural: "Errors",
+			storedInDatabase: true,
+			moreIsBetter: false,
+			type: "attack"
+		},
+		FT: {
+			singular: "40/20",
+			plural: "40/20s",
+			storedInDatabase: true,
+			moreIsBetter: true,
+			type: "attack"
+		},
+		OF: {
+			singular: "Offload",
+			plural: "Offloads",
+			storedInDatabase: true,
+			moreIsBetter: true,
+			type: "attack"
+		}
+	},
+	defence: {
+		TK: {
+			singular: "Tackle",
+			plural: "Tackles",
+			storedInDatabase: true,
+			moreIsBetter: true,
+			type: "defence"
+		},
+		MI: {
+			singular: "Missed Tackle",
+			plural: "Missed Tackles",
+			storedInDatabase: true,
+			moreIsBetter: false,
+			type: "defence"
+		},
+		TS: {
+			singular: "Tackle Success",
+			plural: "Tackle Success",
+			unit: "%",
+			storedInDatabase: false,
+			moreIsBetter: true,
+			type: "defence"
+		},
+		MT: {
+			singular: "Marker Tackle",
+			plural: "Marker Tackles",
+			storedInDatabase: true,
+			moreIsBetter: true,
+			type: "defence"
+		},
+		P: {
+			singular: "Penalty",
+			plural: "Penalties",
+			storedInDatabase: true,
+			moreIsBetter: false,
+			type: "defence"
+		},
+		YC: {
+			singular: "Yellow Card",
+			plural: "Yellow Cards",
+			storedInDatabase: true,
+			moreIsBetter: false,
+			type: "defence"
+		},
+		RC: {
+			singular: "Red Card",
+			plural: "Red Cards",
+			storedInDatabase: true,
+			moreIsBetter: false,
+			type: "defence"
+		}
+	}
+};
--- models/rugby/PlayerStatsCollection.js
+++ models/rugby/PlayerStatsCollection.js
@@ -1,34 +1,16 @@
+const _ = require("lodash");
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
+const playerStatTypes = require("../../constants/playerStatTypes");
 
-const playerStatsCollectionSchema = new Schema({
-	scoring: {
-		T: { type: Number, default: 0 },
-		CN: { type: Number, default: 0 },
-		PK: { type: Number, default: 0 },
-		DG: { type: Number, default: 0 },
-		MG: { type: Number, default: 0 },
-		TA: { type: Number, default: null }
-	},
-	attack: {
-		TB: { type: Number, default: null },
-		AT: { type: Number, default: null },
-		C: { type: Number, default: null },
-		M: { type: Number, default: null },
-		CB: { type: Number, default: null },
-		DR: { type: Number, default: null },
-		E: { type: Number, default: null },
-		FT: { type: Number, default: null },
-		OF: { type: Number, default: null }
-	},
-	defence: {
-		TK: { type: Number, default: null },
-		MT: { type: Number, default: null },
-		MI: { type: Number, default: null },
-		P: { type: Number, default: null },
-		YC: { type: Number, default: null },
-		DC: { type: Number, default: null }
-	}
-});
+const playerStatsCollectionSchema = new Schema(
+	//The first mapValues allows us to filter by category (scoring/attack/defence)
+	_.mapValues(playerStatTypes, stats => {
+		//The second mapValues pulls the stat's initials as a field, and sets it to be a number
+		return _.mapValues(stats, stat => {
+			return { type: Number, default: null };
+		});
+	})
+);
 
 module.exports = playerStatsCollectionSchema;
