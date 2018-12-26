48f81a5d2da9de0b1005143766687cab40606faf
Mark Wilkinson <markwilko22@gmail.com>
2018-12-26 16:39:16 +0000

    Refactored reusable pipelines into another file

--- controllers/rugby/gamesController.js
+++ controllers/rugby/gamesController.js
@@ -1,7 +1,11 @@
 const _ = require("lodash");
 const mongoose = require("mongoose");
-const { localTeam } = require("../../config/keys");
 const Game = mongoose.model("games");
+const {
+	getBasicGameData,
+	getCompetitionInfo,
+	exportBasicInfoOnly
+} = require("../../pipelines/rugby/gamesPipelines");
 const ObjectId = mongoose.Types.ObjectId;
 
 function buildQuery(params) {
@@ -33,195 +37,6 @@ function buildQuery(params) {
 	return query;
 }
 
-const pipelines = {};
-pipelines.competitionInfo = [
-	{
-		$lookup: {
-			from: "competitionsegments",
-			localField: "_competition",
-			foreignField: "_id",
-			as: "_competition"
-		}
-	},
-	{
-		$unwind: "$_competition"
-	},
-	{
-		$lookup: {
-			from: "competitions",
-			localField: "_competition._parentCompetition",
-			foreignField: "_id",
-			as: "_competition._parentCompetition"
-		}
-	},
-	{
-		$unwind: "$_competition._parentCompetition"
-	},
-	{
-		$addFields: {
-			year: {
-				$year: "$date"
-			}
-		}
-	},
-	{
-		$addFields: {
-			"_competition.instances": {
-				$filter: {
-					input: "$_competition.instances",
-					as: "instance",
-					cond: {
-						$or: [
-							{ $eq: ["$$instance.year", "$year"] },
-							{ $eq: ["$$instance.year", null] }
-						]
-					}
-				}
-			}
-		}
-	},
-	{
-		$unwind: "$_competition.instances"
-	},
-	{
-		$addFields: {
-			"_competition.sponsor": {
-				$cond: {
-					if: "$_competition.instances.sponsor",
-					then: { $concat: ["$_competition.instances.sponsor", " "] },
-					else: ""
-				}
-			}
-		}
-	},
-	{
-		$addFields: {
-			"_competition.name": {
-				$cond: {
-					if: "$_competition.appendCompetitionName",
-					then: {
-						$concat: [
-							"$_competition.sponsor",
-							"$_competition._parentCompetition.name",
-							" ",
-							"$_competition.name"
-						]
-					},
-					else: {
-						$concat: ["$_competition.sponsor", "$_competition._parentCompetition.name"]
-					}
-				}
-			}
-		}
-	}
-];
-pipelines.gameData = _.concat(
-	//Get Competition Info
-	pipelines.competitionInfo,
-	[
-		//Get Team Info
-		{
-			$lookup: {
-				from: "teams",
-				localField: "_opposition",
-				foreignField: "_id",
-				as: "_opposition"
-			}
-		},
-		{
-			$unwind: "$_opposition"
-		},
-		{
-			$addFields: {
-				teams: {
-					home: {
-						$cond: {
-							if: "$isAway",
-							then: "$_opposition._id",
-							else: localTeam
-						}
-					},
-					away: {
-						$cond: {
-							if: "$isAway",
-							then: localTeam,
-							else: "$_opposition._id"
-						}
-					}
-				}
-			}
-		},
-
-		//Get Ground Info
-		{
-			$lookup: {
-				from: "grounds",
-				localField: "_ground",
-				foreignField: "_id",
-				as: "_ground"
-			}
-		},
-		{
-			$unwind: "$_ground"
-		},
-		{
-			$lookup: {
-				from: "cities",
-				localField: "_ground.address._city",
-				foreignField: "_id",
-				as: "_ground.address._city"
-			}
-		},
-		{
-			$unwind: "$_ground.address._city"
-		},
-		{
-			$addFields: {
-				title: {
-					$cond: {
-						if: {
-							$ne: ["$title", null]
-						},
-						then: "$title",
-						else: {
-							$cond: {
-								if: {
-									$eq: ["$round", null]
-								},
-								then: "$_competition.name",
-								else: {
-									$concat: [
-										"$_competition.name",
-										" Round ",
-										{ $toLower: "$round" } //TODO Add special rounds
-									]
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-	]
-);
-pipelines.basicInfoOnly = [
-	{
-		$project: {
-			_id: 1,
-			isAway: 1,
-			date: 1,
-			slug: 1,
-			title: 1,
-			"_opposition.colours": 1,
-			"_opposition.name": 1,
-			"_opposition.image": 1,
-			"_ground.address._city": 1,
-			"_ground.name": 1,
-			"_ground.image": 1
-		}
-	}
-];
-
 module.exports = {
 	async getFixtures(req, res) {
 		const query = {
@@ -239,8 +54,8 @@ module.exports = {
 						}
 					}
 				],
-				pipelines.gameData,
-				pipelines.basicInfoOnly
+				getBasicGameData,
+				exportBasicInfoOnly
 			)
 		);
 		res.send(games);
@@ -259,8 +74,8 @@ module.exports = {
 						}
 					}
 				],
-				pipelines.gameData,
-				pipelines.basicInfoOnly
+				getBasicGameData,
+				exportBasicInfoOnly
 			)
 		);
 		res.send(games);
@@ -295,7 +110,7 @@ module.exports = {
 		const competitions = await Game.aggregate(
 			_.concat(
 				[{ $match: query }],
-				pipelines.competitionInfo,
+				getCompetitionInfo,
 
 				[
 					{
---
+++ pipelines/rugby/gamesPipelines.js
@@ -0,0 +1,196 @@
+const _ = require("lodash");
+const { localTeam } = require("../../config/keys");
+
+const getCompetitionInfo = [
+	{
+		$lookup: {
+			from: "competitionsegments",
+			localField: "_competition",
+			foreignField: "_id",
+			as: "_competition"
+		}
+	},
+	{
+		$unwind: "$_competition"
+	},
+	{
+		$lookup: {
+			from: "competitions",
+			localField: "_competition._parentCompetition",
+			foreignField: "_id",
+			as: "_competition._parentCompetition"
+		}
+	},
+	{
+		$unwind: "$_competition._parentCompetition"
+	},
+	{
+		$addFields: {
+			year: {
+				$year: "$date"
+			}
+		}
+	},
+	{
+		$addFields: {
+			"_competition.instances": {
+				$filter: {
+					input: "$_competition.instances",
+					as: "instance",
+					cond: {
+						$or: [
+							{ $eq: ["$$instance.year", "$year"] },
+							{ $eq: ["$$instance.year", null] }
+						]
+					}
+				}
+			}
+		}
+	},
+	{
+		$unwind: "$_competition.instances"
+	},
+	{
+		$addFields: {
+			"_competition.sponsor": {
+				$cond: {
+					if: "$_competition.instances.sponsor",
+					then: { $concat: ["$_competition.instances.sponsor", " "] },
+					else: ""
+				}
+			}
+		}
+	},
+	{
+		$addFields: {
+			"_competition.name": {
+				$cond: {
+					if: "$_competition.appendCompetitionName",
+					then: {
+						$concat: [
+							"$_competition.sponsor",
+							"$_competition._parentCompetition.name",
+							" ",
+							"$_competition.name"
+						]
+					},
+					else: {
+						$concat: ["$_competition.sponsor", "$_competition._parentCompetition.name"]
+					}
+				}
+			}
+		}
+	}
+];
+const getBasicGameData = _.concat(
+	//Get Competition Info
+	getCompetitionInfo,
+	[
+		//Get Team Info
+		{
+			$lookup: {
+				from: "teams",
+				localField: "_opposition",
+				foreignField: "_id",
+				as: "_opposition"
+			}
+		},
+		{
+			$unwind: "$_opposition"
+		},
+		{
+			$addFields: {
+				teams: {
+					home: {
+						$cond: {
+							if: "$isAway",
+							then: "$_opposition._id",
+							else: localTeam
+						}
+					},
+					away: {
+						$cond: {
+							if: "$isAway",
+							then: localTeam,
+							else: "$_opposition._id"
+						}
+					}
+				}
+			}
+		},
+
+		//Get Ground Info
+		{
+			$lookup: {
+				from: "grounds",
+				localField: "_ground",
+				foreignField: "_id",
+				as: "_ground"
+			}
+		},
+		{
+			$unwind: "$_ground"
+		},
+		{
+			$lookup: {
+				from: "cities",
+				localField: "_ground.address._city",
+				foreignField: "_id",
+				as: "_ground.address._city"
+			}
+		},
+		{
+			$unwind: "$_ground.address._city"
+		},
+		{
+			$addFields: {
+				title: {
+					$cond: {
+						if: {
+							$ne: ["$title", null]
+						},
+						then: "$title",
+						else: {
+							$cond: {
+								if: {
+									$eq: ["$round", null]
+								},
+								then: "$_competition.name",
+								else: {
+									$concat: [
+										"$_competition.name",
+										" Round ",
+										{ $toLower: "$round" } //TODO Add special rounds
+									]
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	]
+);
+const exportBasicInfoOnly = [
+	{
+		$project: {
+			_id: 1,
+			isAway: 1,
+			date: 1,
+			slug: 1,
+			title: 1,
+			"_opposition.colours": 1,
+			"_opposition.name": 1,
+			"_opposition.image": 1,
+			"_ground.address._city": 1,
+			"_ground.name": 1,
+			"_ground.image": 1
+		}
+	}
+];
+
+module.exports = {
+	getCompetitionInfo,
+	getBasicGameData,
+	exportBasicInfoOnly
+};
