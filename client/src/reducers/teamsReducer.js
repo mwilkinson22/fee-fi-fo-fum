6e041d3c579d033e38c13bb5131280266cabf196
Mark Wilkinson <markwilko22@gmail.com>
2019-01-07 14:48:01 +0000

    Tweaked teamlist to prevent displaying 2019 squadnumbers

--- client/src/reducers/teamsReducer.js
+++ client/src/reducers/teamsReducer.js
@@ -1,11 +1,20 @@
 import { FETCH_SQUAD, FETCH_YEARS_WITH_SQUADS } from "../actions/types";
+import _ from "lodash";
 
 export default function(state = { squads: {} }, action) {
 	switch (action.type) {
 		case FETCH_SQUAD:
+			let { players } = action.payload;
+			if (process.env.NODE_ENV === "production" && action.payload.year == 2019) {
+				//TODO remove once squadnumbers are announced
+				players = _.map(players, player => {
+					player.number = null;
+					return player;
+				});
+			}
 			return {
 				...state,
-				squads: { ...state.squads, [action.payload.year]: action.payload.players }
+				squads: { ...state.squads, [action.payload.year]: players }
 			};
 		case FETCH_YEARS_WITH_SQUADS:
 			return {
