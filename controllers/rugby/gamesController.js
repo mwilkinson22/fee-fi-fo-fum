c2af14cb24d9dc3b6b6ee075995265d9bb81ff8a
Mark Wilkinson <markwilko22@gmail.com>
2019-01-18 12:36:24 +0000

    Cleaned up GamesController, removing old methods

--- controllers/rugby/gamesController.js
+++ controllers/rugby/gamesController.js
@@ -1,49 +1,13 @@
 const _ = require("lodash");
 const mongoose = require("mongoose");
 const Game = mongoose.model("games");
-const {
-	getBasicGameData,
-	getCompetitionInfo,
-	projections
-} = require("../../pipelines/rugby/gamesPipelines");
+const { getBasicGameData, projections } = require("../../pipelines/rugby/gamesPipelines");
 const { ObjectId } = require("mongodb");
-const { earliestGiantsData } = require("../../config/keys");
-
-function buildQuery(params) {
-	const query = {};
-	if (params.year) {
-		//Only fired for "results"
-		const year = Number(params.year);
-		const now = new Date();
-		const endOfYear = new Date(`${year + 1}-01-01`);
-		const latestDate = now < endOfYear ? now : endOfYear;
-		query.date = {
-			$gte: new Date(`${year}-01-01`),
-			$lt: latestDate
-		};
-	}
-
-	if (params.opposition) {
-		query._opposition = ObjectId(params.opposition);
-	}
-
-	if (params.venue) {
-		query.isAway = params.venue === "a";
-	}
-
-	if (params.competitions) {
-		query._competition = ObjectId(params.competitions);
-	}
-
-	return query;
-}
 
 async function aggregateForList(initialPipelines) {
 	const games = await Game.aggregate(
 		_.concat(initialPipelines, getBasicGameData, { $project: projections.basic })
 	);
-	console.log("hi");
-
 	return _.map(games, game => getScores(game));
 }
 
@@ -65,36 +29,6 @@ function getScores(game) {
 }
 
 module.exports = {
-	async getFixtures(req, res) {
-		const query = {
-			date: { $gt: new Date() },
-			...buildQuery(req.query)
-		};
-		const games = await aggregateForList([
-			{ $match: query },
-			{
-				$sort: {
-					date: 1
-				}
-			}
-		]);
-		res.send(games);
-	},
-
-	async getResults(req, res) {
-		const { year } = req.params;
-		const query = buildQuery({ ...req.query, year });
-		const games = await aggregateForList([
-			{ $match: query },
-			{
-				$sort: {
-					date: -1
-				}
-			}
-		]);
-		res.send(games);
-	},
-
 	async getGames(req, res) {
 		const { year, teamType } = req.params;
 		const query = {};
@@ -225,47 +159,6 @@ module.exports = {
 		res.send(games);
 	},
 
-	async getGamesByYear(req, res) {
-		const { year } = req.params;
-		let games;
-		if (year === "fixtures") {
-			games = await aggregateForList([
-				{
-					$match: {
-						date: { $gt: new Date() }
-					}
-				},
-				{
-					$sort: {
-						date: -1
-					}
-				}
-			]);
-		} else {
-			const yearAsNumber = Number(year);
-			const now = new Date();
-			const endOfYear = new Date(`${yearAsNumber + 1}-01-01`);
-			const latestDate = now < endOfYear ? now : endOfYear;
-			games = await aggregateForList([
-				{
-					$match: {
-						date: {
-							$gte: new Date(`${yearAsNumber}-01-01`),
-							$lt: latestDate
-						}
-					}
-				},
-				{
-					$sort: {
-						date: 1
-					}
-				}
-			]);
-		}
-
-		res.send({ year, games });
-	},
-
 	async getFrontpageGames(req, res) {
 		const lastGame = await Game.findOne({}, "_id")
 			.getFixtures(false)
@@ -303,110 +196,5 @@ module.exports = {
 		]);
 
 		res.send(results);
-	},
-
-	async getYearsWithResults(req, res) {
-		const years = await Game.aggregate([
-			{ $sort: { date: 1 } },
-			{
-				$match: {
-					$and: [
-						{ date: { $lt: new Date() } },
-						{ date: { $gte: new Date(`${earliestGiantsData}-01-01`) } }
-					]
-				}
-			},
-			{ $group: { _id: { $year: "$date" } } }
-		]);
-		res.send(years.map(year => year._id));
-	},
-
-	async getFilters(req, res) {
-		const { year } = req.params;
-
-		//Create query
-		const query = {};
-		if (year === "fixtures") {
-			query.date = {
-				$gte: new Date()
-			};
-		} else {
-			query.date = {
-				$gte: new Date(year + "-01-01"),
-				$lt: new Date(Number(year) + 1 + "-01-01")
-			};
-		}
-
-		//Get competitions
-		const competitions = await Game.aggregate(
-			_.concat(
-				[{ $match: query }],
-				getCompetitionInfo,
-
-				[
-					{
-						$group: {
-							_id: "$_competition._id",
-							name: { $first: "$_competition.name" }
-						}
-					},
-					{
-						$sort: {
-							name: 1
-						}
-					}
-				]
-			)
-		);
-
-		//Get Opposition
-		const opposition = await Game.aggregate([
-			{ $match: query },
-			{
-				$lookup: {
-					from: "teams",
-					localField: "_opposition",
-					foreignField: "_id",
-					as: "_opposition"
-				}
-			},
-			{ $group: { _id: "$_opposition._id", name: { $first: "$_opposition.name.long" } } },
-			{ $sort: { name: 1 } }
-		]);
-
-		const venue = await Game.aggregate([
-			{ $match: query },
-			{
-				$project: {
-					_id: {
-						$cond: {
-							if: "$isAway",
-							then: "a",
-							else: "h"
-						}
-					},
-					name: {
-						$cond: {
-							if: "$isAway",
-							then: "Away",
-							else: "Home"
-						}
-					}
-				}
-			},
-			{
-				$group: { _id: "$_id", name: { $first: "$name" } }
-			},
-			{
-				$sort: {
-					name: -1
-				}
-			}
-		]);
-		res.send({
-			competitions,
-			opposition,
-			venue
-		});
 	}
 };
