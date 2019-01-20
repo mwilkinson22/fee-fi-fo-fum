8c852536a092e9dc9d48256b421e0b659d37a056
Mark Wilkinson <markwilko22@gmail.com>
2019-01-20 19:30:14 +0000

    Added Facebook meta tags

--- client/src/components/HelmetBuilder.js
+++ client/src/components/HelmetBuilder.js
@@ -4,7 +4,7 @@ import { localUrl, imagePath } from "../extPaths";
 
 export default class HelmetBuilder extends Component {
 	render() {
-		const { title, canonical, author, cardImage } = this.props;
+		const { title, canonical, author, cardImage, description } = this.props;
 		const fullTitle = (title ? title + " - " : "") + "Fee Fi Fo Fum";
 		document.title = fullTitle;
 		return (
@@ -13,13 +13,19 @@ export default class HelmetBuilder extends Component {
 				<link rel="canonical" href={`${localUrl}/${canonical || ""}`} />
 				<link rel="shortcut icon" href={`${imagePath}favicon.png`} />
 				<meta name="twitter:card" content="summary_large_image" />
-				<meta name="twitter:title" content={fullTitle} />
+				<meta name="twitter:title" property="og:title" content={fullTitle} />
 				<meta name="twitter:site" content="@GiantsFanzine" />
 				<meta name="twitter:creator" content={`@${author || "GiantsFanzine"}`} />
 				<meta
 					name="twitter:image"
+					property="og:image"
 					content={cardImage || imagePath + "layout/twitter-card.jpg"}
 				/>
+				<meta
+					property="twitter:description"
+					content={description || "Huddersfield Giants news, stats and reports"}
+				/>
+				<meta property="og:url" content={`${localUrl}/${canonical || ""}`} />
 			</Helmet>
 		);
 	}
