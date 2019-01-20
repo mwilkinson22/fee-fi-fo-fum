4358b413dfa3b9b62c83919449024c46cc0da963
Mark Wilkinson <markwilko22@gmail.com>
2019-01-20 19:26:03 +0000

    Added Twitter meta image tags

--- client/src/components/HelmetBuilder.js
+++ client/src/components/HelmetBuilder.js
@@ -4,7 +4,7 @@ import { localUrl, imagePath } from "../extPaths";
 
 export default class HelmetBuilder extends Component {
 	render() {
-		const { title, canonical, author } = this.props;
+		const { title, canonical, author, cardImage } = this.props;
 		const fullTitle = (title ? title + " - " : "") + "Fee Fi Fo Fum";
 		document.title = fullTitle;
 		return (
@@ -16,6 +16,10 @@ export default class HelmetBuilder extends Component {
 				<meta name="twitter:title" content={fullTitle} />
 				<meta name="twitter:site" content="@GiantsFanzine" />
 				<meta name="twitter:creator" content={`@${author || "GiantsFanzine"}`} />
+				<meta
+					name="twitter:image"
+					content={cardImage || imagePath + "layout/twitter-card.jpg"}
+				/>
 			</Helmet>
 		);
 	}
