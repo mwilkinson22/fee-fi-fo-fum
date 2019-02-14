a2f4d3060d99c8ee872c989f17932d8565c50a07
Mark Wilkinson <markwilko22@gmail.com>
2019-02-14 13:29:12 +0000

    Added robots info to NotFoundPage

--- client/pages/NotFoundPage.js
+++ client/pages/NotFoundPage.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
+import { Helmet } from "react-helmet";
 
 export default class NotFoundPage extends Component {
 	render() {
+		const { message } = this.props;
 		return (
 			<div className="container">
-				<h1>404 - Page not found</h1>
+				<Helmet>
+					<meta name="robots" content="NOINDEX, NOFOLLOW" />
+				</Helmet>
+				<h1>404 - {message || "Page not found"}</h1>
 			</div>
 		);
 	}
