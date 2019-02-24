b61c4e6810d51d31e93f3b7514a39f2180c04ba1
Mark Wilkinson <markwilko22@gmail.com>
2019-02-24 18:35:21 +0000

    Set up standard form to use formik

--- client/components/admin/standardForm/AdminStandardForm.js
+++ client/components/admin/standardForm/AdminStandardForm.js
@@ -1,114 +1,172 @@
 import _ from "lodash";
 import React, { Component } from "react";
-import { reduxForm, Field } from "redux-form";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import LoadingPage from "../../LoadingPage";
+import * as Yup from "yup";
+import Select from "react-select";
+import Radio from "../fields/Radio";
+import Boolean from "../fields/Boolean";
 
-export default props => {
-	const { fieldGroups, formName, customValidation, isNew } = props;
-
-	class AdminStandardForm extends Component {
-		renderField(field) {
-			const { submitFailed, error } = field.meta;
-			const id = field.input.name;
-			let inputElement;
-
-			switch (field.type) {
-				case "select":
-					inputElement = this.renderSelect(field);
-					break;
-				default:
-					inputElement = (
-						<input
-							key={field.name}
-							type={field.type}
-							{...field.input}
-							value={field.defaultValue}
-						/>
-					);
-					break;
-			}
-			return [
-				<label key="label" htmlFor={id} className={field.required ? "required" : ""}>
-					{field.label}
-				</label>,
-				inputElement,
-				<span key="error" className="error">
-					{submitFailed ? error : ""}
-				</span>
-			];
-		}
-
-		renderSelect(field) {
-			let i = 0;
-			return (
-				<select key="input" {...field.input} value={field.defaultValue}>
-					{_.map(field.options, (value, title) => {
-						return (
-							<option key={i++} value={value}>
-								{title}
-							</option>
-						);
-					})}
-				</select>
-			);
-		}
-
-		renderFieldGroups() {
-			return _.map(fieldGroups, (fields, header) => {
-				const elements = _.map(fields, field => (
-					<Field key={field.name} component={this.renderField.bind(this)} {...field} />
-				));
-				return [<h6 key={header}>{header}</h6>, ...elements];
-			});
-		}
-
-		render() {
-			const { handleSubmit, onSubmit } = this.props;
-			return (
-				<div className="container">
-					<form onSubmit={handleSubmit(onSubmit)}>
-						<div className="form-card">
-							{this.renderFieldGroups()}
-							<div className="buttons">
-								<button type="reset">Reset</button>
-								<button type="submit">{isNew ? "Add" : "Update"}</button>
-							</div>
-						</div>
-					</form>
-				</div>
-			);
-		}
+export default class AdminStandardForm extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
 	}
 
-	//Helper Methods
-	function validate(values) {
-		const errors = {};
-		_.map(fieldGroups, fields => {
-			_.map(fields, field => {
-				const { name, required, type } = field;
+	static getDerivedStateFromProps(nextProps, prevState) {
+		const { fieldGroups, onSubmit } = nextProps;
+		return { fieldGroups, onSubmit };
+	}
 
-				//Check for empty required fields
-				if (required) {
-					let isValid = false;
-					switch (type) {
-						case "select":
-							break;
-						default:
-							isValid = values[name];
-					}
+	getInitialValues() {
+		const initialValues = {};
+		_.each(this.state.fieldGroups, fields => {
+			_.each(fields, field => (initialValues[field.name] = field.value || ""));
+		});
+		return initialValues;
+	}
 
-					if (!isValid) {
-						errors[name] = "Please enter a value";
-					}
+	renderForm() {
+		const { fieldGroups } = this.state;
+		const content = [];
+		_.each(fieldGroups, (fields, group) => {
+			content.push(<h6 key={group + " title"}>{group}</h6>);
+			_.each(fields, field => {
+				//Check for required fields
+				if (field.validation) {
+					field.required =
+						typeof _.find(
+							field.validation.describe().tests,
+							test => test.name === "required"
+						) === "object";
 				}
+
+				//Get Core Values
+				const { name, label, required } = field;
+
+				//Add Label
+				content.push(
+					<label
+						key={`${name} label`}
+						htmlFor={name}
+						className={required ? "required" : ""}
+					>
+						{label}
+					</label>
+				);
+
+				//Add Field
+				content.push(this.renderField(field));
+
+				//Add Error Message
+				content.push(
+					<div className="error" key={name + " error"}>
+						<ErrorMessage name={name} />
+					</div>
+				);
 			});
 		});
+		return (
+			<Form>
+				<div className="form-card">
+					{content}
+					<div className="buttons">
+						<button type="reset">Reset Form</button>
+						<button type="submit">Submit</button>
+					</div>
+				</div>
+			</Form>
+		);
+	}
 
-		if (customValidation) {
-			customValidation(values, errors, fieldGroups);
+	renderField(field) {
+		const { name, type, label, disabled, validation } = field;
+		const fieldProps = {
+			key: name + " field",
+			name,
+			title: label,
+			disabled,
+			id: name
+		};
+		switch (field.type) {
+			case "boolean":
+				fieldProps.component = Boolean;
+				break;
+			case "radio":
+				fieldProps.component = Radio;
+				fieldProps.options = field.options;
+				break;
+			case "select":
+				fieldProps.component = Select;
+				fieldProps.isDisabled = field.disabled;
+				fieldProps.options = field.options;
+				fieldProps.styles = {
+					option: (provided, state) => ({
+						...provided,
+						background: state.isSelected
+							? "#751432"
+							: state.isFocused
+								? "#7514324d"
+								: "transparent",
+						":active": {
+							backgroundColor: "#7514324d"
+						}
+					}),
+					control: (provided, state) => ({
+						...provided,
+						borderColor: "#751432",
+						boxShadow: state.isFocused || state.isSelected ? "0 0 0 1px #751432" : null,
+						"&:hover": {
+							borderColor: "#751432"
+						}
+					})
+				};
+				break;
+			default:
+				fieldProps.component = "input";
+				fieldProps.type = type;
+				if (validation) {
+					const tests = _.keyBy(field.validation.describe().tests, "name");
+					//Check for min max
+					if (tests.min) {
+						fieldProps.min = tests.min.params.min;
+					}
+					if (tests.max) {
+						fieldProps.max = tests.max.params.max;
+					}
+				}
+				break;
 		}
 
-		return errors;
+		return <Field {...fieldProps} />;
 	}
-	const Form = reduxForm({ form: formName, validate })(AdminStandardForm);
-	return <Form {...props} />;
-};
+
+	getValidationSchema() {
+		const schema = {};
+		_.each(this.state.fieldGroups, fields => {
+			_.each(fields, field => {
+				const { validation } = field;
+				if (validation) {
+					validation._label = field.label;
+				}
+				schema[field.name] = validation || "";
+			});
+		});
+		return Yup.object().shape(schema);
+	}
+
+	render() {
+		const { fieldGroups, onSubmit } = this.state;
+		if (!fieldGroups || !onSubmit) {
+			return <LoadingPage />;
+		} else {
+			return (
+				<Formik
+					initialValues={this.getInitialValues()}
+					validationSchema={this.getValidationSchema()}
+					render={() => this.renderForm()}
+				/>
+			);
+		}
+	}
+}
