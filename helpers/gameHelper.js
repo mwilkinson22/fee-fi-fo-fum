54b383ee6485c4c7cbed4e9f1c74738453f475f6
Mark Wilkinson <markwilko22@gmail.com>
2019-03-20 15:42:03 +0000

    Refactored date logic into gameHelper

--- helpers/gameHelper.js
+++ helpers/gameHelper.js
@@ -1,3 +1,5 @@
+import _ from "lodash";
+
 export function validateGameDate(game, listType, year = null) {
 	const now = new Date();
 
@@ -6,4 +8,11 @@ export function validateGameDate(game, listType, year = null) {
 	} else {
 		return game.date > now;
 	}
+}
+
+export function fixDates(games) {
+	return _.mapValues(games, game => {
+		game.date = new Date(game.date);
+		return game;
+	});
 }
