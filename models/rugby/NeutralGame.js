44e8959d59f3c33eb89fb3376cc628dedf8c5432
Mark Wilkinson <markwilko22@gmail.com>
2019-04-11 09:31:01 +0100

    Tidied up model

--- models/rugby/NeutralGame.js
+++ models/rugby/NeutralGame.js
@@ -3,15 +3,14 @@ const { Schema } = mongoose;
 
 const neutralGameSchema = new Schema({
 	_competition: { type: Schema.Types.ObjectId, ref: "competitionSegments" },
-	date: Date,
-	_homeTeam: { type: Schema.Types.ObjectId, ref: "teams" },
-	_awayTeam: { type: Schema.Types.ObjectId, ref: "teams" },
+	date: { type: Date, required: true },
+	_homeTeam: { type: Schema.Types.ObjectId, ref: "teams", required: true },
+	_awayTeam: { type: Schema.Types.ObjectId, ref: "teams", required: true },
 	homePoints: { type: Number, default: null },
 	awayPoints: { type: Number, default: null },
-	neutralGround: { type: Boolean, default: false },
 	_teamType: { type: Schema.Types.ObjectId, ref: "teamTypes" },
-	externalId: Number,
-	externalSite: { type: String, enum: ["RFL", "SL", null] }
+	externalId: { type: Number, default: null },
+	externalSite: { type: String, enum: ["RFL", "SL", null], default: null }
 });
 
 mongoose.model("neutralGames", neutralGameSchema);
