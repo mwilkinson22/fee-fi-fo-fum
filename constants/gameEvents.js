602cd2120e19f19bae54ff7aad9c00bc292f3fc4
Mark Wilkinson <markwilko22@gmail.com>
2019-04-18 20:29:36 +0100

    Refactored game events to a constants file

---
+++ constants/gameEvents.js
@@ -0,0 +1,15 @@
+export default {
+	none: { label: "None", isPlayerEvent: false },
+	T: { label: "Try", isPlayerEvent: true },
+	CN: { label: "Conversion", isPlayerEvent: true },
+	PK: { label: "Penalty Goal", isPlayerEvent: true },
+	DG: { label: "Drop Goal", isPlayerEvent: true },
+	YC: { label: "Yellow Card", isPlayerEvent: true },
+	RC: { label: "Red Card", isPlayerEvent: true },
+	motm: { label: "Man of the Match", isPlayerEvent: true },
+	fan_motm: { label: "Fans' Man of the Match", isPlayerEvent: true },
+	kickOff: { label: "Kick Off", isPlayerEvent: false },
+	halfTime: { label: "Half Time", isPlayerEvent: false },
+	fullTime: { label: "Full Time", isPlayerEvent: false },
+	attendance: { label: "Attendance", isPlayerEvent: false }
+};
