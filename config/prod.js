751af851d9219e7369c9068f859d6cec41589095
Mark Wilkinson <markwilko22@gmail.com>
2019-04-18 11:54:07 +0100

    Added Twitter Client

--- config/prod.js
+++ config/prod.js
@@ -3,5 +3,11 @@ module.exports = {
 	cookieKey: process.env.COOKIE_KEY,
 	localTeam: process.env.LOCAL_TEAM,
 	earliestGiantsData: 2017,
-	fixtureCrawlUrl: process.env.FIXTURE_CRAWL_URL
+	fixtureCrawlUrl: process.env.FIXTURE_CRAWL_URL,
+	twitter: {
+		consumer_key: process.env.TWITTER_CONSUMER_KEY,
+		consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+		access_token_key: process.env.TWITTER_ACCESS_KEY,
+		access_token_secret: process.env.TWITTER_ACCESS_SECRET
+	}
 };
