649e6b6613a4e78a1ca9a505353357d907da2d17
Mark Wilkinson <markwilko22@gmail.com>
2019-05-23 14:00:32 +0100

    Created PlayerEventImage

---
+++ images/PlayerEvent.js
@@ -0,0 +1,38 @@
+import Canvas from "./Canvas";
+
+export default class PlayerEvent extends Canvas {
+	constructor(game, options = {}) {
+		//Set Dimensions
+		const cWidth = 1400;
+		const cHeight = cWidth / 2;
+
+		//Load In Fonts
+		const fonts = [
+			{ file: "Montserrat-Bold.ttf", family: "Montserrat" },
+			{ file: "Monstro.ttf", family: "Monstro" }
+		];
+
+		//Create Canvas
+		super(cWidth, cHeight, { fonts });
+
+		//Constants
+		const textStyles = {};
+		this.setTextStyles(textStyles);
+
+		this.positions = {};
+
+		//Variables
+	}
+
+	async drawBackground() {
+		const { ctx, cWidth, cHeight } = this;
+		const backgroundImage = await this.googleToCanvas("images/layout/canvas/player-event.jpg");
+		ctx.drawImage(backgroundImage, 0, 0, cWidth, cHeight);
+	}
+
+	async render(forTwitter = false) {
+		await this.drawBackground();
+
+		return this.outputFile(forTwitter ? "twitter" : "base64");
+	}
+}
