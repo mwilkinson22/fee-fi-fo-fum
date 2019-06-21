e6ec126b400de4fc15e2b219c9ecc5ca56a63776
Mark Wilkinson <markwilko22@gmail.com>
2019-06-21 13:48:21 +0100

    Missed Goals defaults to 0

--- models/rugby/PlayerStatsCollection.js
+++ models/rugby/PlayerStatsCollection.js
@@ -6,7 +6,7 @@ const playerStatTypes = require("../../constants/playerStatTypes");
 const playerStatsCollectionSchema = new Schema(
 	//Adds all stat keys to schema as Numbers
 	_.mapValues(playerStatTypes, (content, key) => {
-		const defaultZero = ["T", "CN", "PK", "DG", "YC", "RC"];
+		const defaultZero = ["T", "CN", "PK", "DG", "MG", "YC", "RC"];
 		return { type: Number, default: defaultZero.indexOf(key) === -1 ? null : 0 };
 	})
 );
