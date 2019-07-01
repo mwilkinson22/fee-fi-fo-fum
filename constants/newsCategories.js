81b104b9f88257329806d4f7151208908945ec9a
Mark Wilkinson <markwilko22@gmail.com>
2019-07-01 11:21:52 +0100

    Removed quiz category

--- constants/newsCategories.js
+++ constants/newsCategories.js
@@ -2,6 +2,5 @@ export default [
 	{ name: "Previews", slug: "previews" },
 	{ name: "Recaps", slug: "recaps" },
 	{ name: "Features", slug: "features" },
-	{ name: "Opinions", slug: "opinions" },
-	{ name: "Quizzes", slug: "quizzes" }
+	{ name: "Opinions", slug: "opinions" }
 ];
