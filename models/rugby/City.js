f42163ef1896967f8452c54a6411b02d6d459568
Mark Wilkinson <markwilko22@gmail.com>
2019-07-19 11:34:24 +0100

    Added slug generation to locations

--- models/rugby/City.js
+++ models/rugby/City.js
@@ -2,8 +2,41 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const citySchema = new Schema({
-	name: String,
-	_country: { type: Schema.Types.ObjectId, ref: "countries" }
+	name: { type: String, unique: true, required: true },
+	_country: { type: Schema.Types.ObjectId, ref: "countries" },
+	slug: { type: String, unique: true, required: true }
 });
+
+citySchema.statics.generateSlug = async function({ name, _country }) {
+	//Get Country
+	const Country = mongoose.model("countries");
+	const country = await Country.findById(_country, "slug");
+
+	const citySlug = name
+		.toLowerCase()
+		.replace(/\s+/gi, "-")
+		.replace(/(?![a-z-])./gi, "");
+
+	const coreSlugText = `${citySlug}-${country.slug}`;
+
+	let slugExists = await this.findOne({
+		slug: coreSlugText
+	});
+
+	if (!slugExists) {
+		return coreSlugText;
+	} else {
+		let i = 2;
+		let slug;
+		while (slugExists) {
+			slug = coreSlugText + "-" + i++;
+			slugExists = await this.findOne({
+				slug
+			});
+		}
+
+		return slug;
+	}
+};
 
 mongoose.model("cities", citySchema);
--- models/rugby/Country.js
+++ models/rugby/Country.js
@@ -2,8 +2,35 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const countrySchema = new Schema({
-	name: String,
-	demonym: String
+	name: { type: String, unique: true, required: true },
+	demonym: { type: String, unique: true, required: true },
+	slug: { type: String, unique: true, required: true }
 });
+
+countrySchema.statics.generateSlug = async function(name) {
+	const coreSlugText = name
+		.toLowerCase()
+		.replace(/\s+/gi, "-")
+		.replace(/(?![a-z-])./gi, "");
+
+	let slugExists = await this.findOne({
+		slug: coreSlugText
+	});
+
+	if (!slugExists) {
+		return coreSlugText;
+	} else {
+		let i = 2;
+		let slug;
+		while (slugExists) {
+			slug = coreSlugText + "-" + i++;
+			slugExists = await this.findOne({
+				slug
+			});
+		}
+
+		return slug;
+	}
+};
 
 mongoose.model("countries", countrySchema);
