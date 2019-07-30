7947de215f9de72bdba05bfa84814c92c129d7fa
Mark Wilkinson <markwilko22@gmail.com>
2019-07-30 14:49:32 +0100

    Added url to PlayerSponsor

--- models/PlayerSponsor.js
+++ models/PlayerSponsor.js
@@ -3,6 +3,7 @@ const { Schema } = mongoose;
 
 const sponsorSchema = new Schema({
 	name: { type: String, unique: true, required: true },
+	url: { type: String, default: null },
 	twitter: { type: String, default: null },
 	image: { type: String, default: null }
 });
