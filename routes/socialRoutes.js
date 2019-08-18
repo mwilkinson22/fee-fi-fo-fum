21bb90c8c2f232c637dee0a60b0e2f0c4297d591
Mark Wilkinson <markwilko22@gmail.com>
2019-08-18 09:22:38 +0100

    Removed temp twitter routes

--- routes/socialRoutes.js
+++ routes/socialRoutes.js
@@ -2,9 +2,6 @@ import * as SocialController from "../controllers/socialController";
 import requireAdmin from "../middlewares/requireAdmin";
 
 module.exports = app => {
-	app.get("/api/twitter/auth", SocialController.twitterAuth);
-	app.get("/api/twitter/callback", SocialController.twitterCallback);
-
 	app.post("/api/socialProfiles/twitterTest", requireAdmin, SocialController.twitterTest);
 	app.post("/api/socialProfiles", requireAdmin, SocialController.createProfile);
 	app.get("/api/socialProfiles", requireAdmin, SocialController.getProfiles);
