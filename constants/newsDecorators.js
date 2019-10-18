e7fd8bf5d595978603bdbd9fe4e04c9671973a28
Mark Wilkinson <markwilko22@gmail.com>
2019-10-18 07:42:01 +0100

    Fixed Internal Link

--- constants/newsDecorators.js
+++ constants/newsDecorators.js
@@ -5,7 +5,7 @@ import Link from "megadraft/lib/components/Link";
 //Components
 import InternalLink from "~/client/components/news/entities/InternalLink";
 
-const decorators = [["LINK", Link], [("INTERNAL_PAGE_LINK", InternalLink)]];
+const decorators = [["LINK", Link], ["INTERNAL_PAGE_LINK", InternalLink]];
 
 export default new DraftJS.CompositeDecorator(
 	decorators.map(([entity, component]) => ({
