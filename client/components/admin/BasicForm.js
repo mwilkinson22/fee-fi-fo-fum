8a02015def1119292f219802d820d3c86721a397
Mark Wilkinson <markwilko22@gmail.com>
2019-11-07 08:10:12 +0000

    Added prop types

--- client/components/admin/BasicForm.js
+++ client/components/admin/BasicForm.js
@@ -1,10 +1,15 @@
 //Modules
+import _ from "lodash";
 import { Component } from "react";
+import PropTypes from "prop-types";
+
+//Constants
+import * as fieldTypes from "~/constants/formFieldTypes";
 
 //Helpers
 import { renderFieldGroup as rFG } from "~/helpers/formHelper";
 
-export default class BasicForm extends Component {
+class BasicForm extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {};
@@ -19,3 +24,33 @@ export default class BasicForm extends Component {
 		return null;
 	}
 }
+
+BasicForm.propTypes = {
+	fastFieldByDefault: PropTypes.bool,
+	fieldGroups: PropTypes.arrayOf(
+		PropTypes.shape({
+			label: PropTypes.string,
+			fields: PropTypes.arrayOf(
+				PropTypes.shape({
+					name: PropTypes.string.isRequired,
+					type: PropTypes.oneOf(_.values(fieldTypes))
+				})
+			).isRequired
+		})
+	).isRequired,
+	itemType: PropTypes.string.isRequired,
+	initialValues: PropTypes.object.isRequired,
+	onSubmit: PropTypes.func.isRequired, //values => Action(id, values)
+	onDelete: PropTypes.func, // Action
+	//Either be a simple string, or a callback passing in form values and action result
+	redirectOnSubmit: PropTypes.oneOfType([PropTypes.string, PropTypes.func]),
+	redirectOnDelete: PropTypes.string,
+	validationSchema: PropTypes.object().isRequired
+};
+
+BasicForm.defaultProps = {
+	fastFieldByDefault: true,
+	redirectOnDelete: `/admin/`
+};
+
+export default BasicForm;
