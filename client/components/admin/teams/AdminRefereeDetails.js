13884a41eea4af5328c3487224ca0dd40fc0fa8e
Mark Wilkinson <markwilko22@gmail.com>
2019-11-26 08:24:35 +0000

    Added BasicForm to referee details

--- client/components/admin/teams/AdminRefereeDetails.js
+++ client/components/admin/teams/AdminRefereeDetails.js
@@ -1,8 +1,7 @@
 //Modules
 import _ from "lodash";
-import React from "react";
+import React, { Component } from "react";
 import { connect } from "react-redux";
-import { Formik, Form } from "formik";
 import * as Yup from "yup";
 
 //Actions
@@ -14,7 +13,7 @@ import BasicForm from "../BasicForm";
 //Constants
 import * as fieldTypes from "~/constants/formFieldTypes";
 
-class AdminRefereeDetails extends BasicForm {
+class AdminRefereeDetails extends Component {
 	constructor(props) {
 		super(props);
 
@@ -31,62 +30,49 @@ class AdminRefereeDetails extends BasicForm {
 
 		const newState = {};
 
-		const { slug } = match.params;
-		newState.person = _.find(fullPeople, p => p.slug == slug);
+		const { _id } = match.params;
+		newState.person = fullPeople[_id];
 
 		return newState;
 	}
 
-	getDefaults() {
+	getInitialValues() {
 		const { refereeDetails } = this.state.person;
 
-		const defaults = {
+		const defaultValues = {
 			from: "",
 			to: ""
 		};
-		return _.mapValues(defaults, (val, key) =>
+		return _.mapValues(defaultValues, (defaultValue, key) =>
 			refereeDetails && refereeDetails[key]
 				? new Date(refereeDetails[key]).toString("yyyy-MM-dd")
-				: ""
+				: defaultValue
 		);
 	}
 
-	async onSubmit(refereeDetails) {
-		const { person } = this.state;
-		const { updatePerson } = this.props;
-
-		updatePerson(person._id, { refereeDetails });
+	getFieldGroups() {
+		return [
+			{
+				fields: [
+					{ name: "from", type: fieldTypes.date },
+					{ name: "to", type: fieldTypes.date }
+				]
+			}
+		];
 	}
 
 	render() {
+		const { updatePerson } = this.props;
+		const { person, validationSchema } = this.state;
 		return (
-			<div className="container">
-				<Formik
-					validationSchema={this.state.validationSchema}
-					onSubmit={values => this.onSubmit(values)}
-					initialValues={this.getDefaults()}
-					render={() => {
-						const fields = [
-							{ name: "from", type: fieldTypes.date },
-							{ name: "to", type: fieldTypes.date }
-						];
-
-						return (
-							<Form>
-								<div className="form-card grid">
-									{this.renderFieldGroup(fields)}
-									<div className="buttons">
-										<button type="clear">Clear</button>
-										<button type="submit" className="confirm">
-											Save
-										</button>
-									</div>
-								</div>
-							</Form>
-						);
-					}}
-				/>
-			</div>
+			<BasicForm
+				fieldGroups={this.getFieldGroups()}
+				initialValues={this.getInitialValues()}
+				isNew={false}
+				itemType="Referee Details"
+				onSubmit={refereeDetails => updatePerson(person._id, { refereeDetails })}
+				validationSchema={validationSchema}
+			/>
 		);
 	}
 }
@@ -97,7 +83,4 @@ function mapStateToProps({ people }) {
 	return { fullPeople };
 }
 // export default form;
-export default connect(
-	mapStateToProps,
-	{ updatePerson }
-)(AdminRefereeDetails);
+export default connect(mapStateToProps, { updatePerson })(AdminRefereeDetails);
