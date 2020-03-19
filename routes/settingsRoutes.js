da69d276c3a7961afdf9987b2b979a5d8237de49
Mark Wilkinson <markwilko22@gmail.com>
2020-03-19 11:05:19 +0000

    Added requireAdmin to settings creation

--- routes/settingsRoutes.js
+++ routes/settingsRoutes.js
@@ -1,6 +1,7 @@
 import * as settingsController from "../controllers/settingsController";
+import requireAdmin from "../middlewares/requireAdmin";
 
 export default app => {
 	app.get("/api/settings/:names", settingsController.getSettings);
-	app.post("/api/settings", settingsController.setSettings);
+	app.post("/api/settings", requireAdmin, settingsController.setSettings);
 };
