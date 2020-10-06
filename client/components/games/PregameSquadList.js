d9e2ea65789336244f21da3abab6b27af03bd7f3
Mark Wilkinson <markwilko22@gmail.com>
2020-10-06 13:34:25 +0100

    Added message when we only have one pregame squad

--- client/components/games/PregameSquadList.js
+++ client/components/games/PregameSquadList.js
@@ -18,157 +18,197 @@ class PregameSquadList extends Component {
 
 	static getDerivedStateFromProps(nextProps) {
 		const { game, previousGame } = nextProps;
-		return { game, previousGame };
+		const newState = { game, previousGame };
+
+		return newState;
 	}
 
-	renderDueDate() {
+	getSquadList(id, previousGame = false) {
+		const { localTeam } = this.props;
+
+		//Only get prevgame for local team
+		if (previousGame && id != localTeam) {
+			return false;
+		}
+
+		//Get the correct game
+		const game = previousGame ? this.state.previousGame : this.state.game;
+		if (!game || !game.pregameSquads) {
+			return false;
+		}
+
+		//Grab squads
+		const squadObject = game.pregameSquads.find(({ _team }) => _team == id);
+		if (squadObject && squadObject.squad && squadObject.squad.length) {
+			return squadObject.squad;
+		} else {
+			return false;
+		}
+	}
+
+	renderDueDate(singleTeam) {
+		const { fullTeams, localTeam } = this.props;
 		const { game } = this.state;
 
 		//Create due date - 2 days before kick off
 		const dueDate = new Date(game.date).addDays(-2);
 
-		//Get string
+		//Get time as string
 		const dateStringObject = getDateString(dueDate);
 		const dateString = dateStringObject.status === "past" ? "soon" : dateStringObject.string;
 
+		//Create full string
+		let str;
+		if (singleTeam) {
+			const teamObject = singleTeam == localTeam ? fullTeams[localTeam] : game._opposition;
+			str = `${teamObject.name.short} squad due ${dateString}`;
+		} else {
+			str = `Squads due ${dateString}`;
+		}
+
 		return (
-			<div className="container">
-				<h2 className="squads-due-message">Squads due {dateString}</h2>
+			<div className="container" key="due-date">
+				<h2 className="squads-due-message">{str}</h2>
 			</div>
 		);
 	}
 
 	renderSquads() {
-		const { game, previousGame } = this.state;
+		const { game } = this.state;
 		const { localTeam, fullTeams } = this.props;
 		let teams = [fullTeams[localTeam], game._opposition];
 		if (game.isAway) {
 			teams = teams.reverse();
 		}
-		const content = _.chain(teams)
-			.map(team => {
-				//Get Team Squad Object
-				const squadObject = game.pregameSquads.find(({ _team }) => _team == team._id);
-				if (!squadObject || !squadObject.squad || !squadObject.squad.length) {
-					return null;
-				}
+		const teamsWithSquads = teams.filter(team => this.getSquadList(team._id));
 
-				//Get Previous Games squad
-				let previousSquad;
-				if (team._id == localTeam && previousGame) {
-					const previousSquadObject = previousGame.pregameSquads.find(
-						({ _team }) => _team == team._id
-					);
-					if (
-						previousSquadObject &&
-						previousSquadObject.squad &&
-						previousSquadObject.squad.length
-					) {
-						previousSquad = previousSquadObject.squad;
-					}
-				}
-
-				//If at least one player has a squad number, we set this to true
-				let includeNumbers = false;
-
-				//Get Players
-				const squad = _.chain(squadObject.squad)
-					.map(p => {
-						const { _player, number } = _.find(
-							game.eligiblePlayers[team._id],
-							({ _player }) => _player._id == p
-						);
-
-						if (number) {
-							includeNumbers = true;
-						}
-
-						return {
-							id: _player._id,
-							name: `${_player.name.first} ${_player.name.last}`,
-							number,
-							slug: _player.slug,
-							isNew: previousSquad && !previousSquad.find(id => id == _player._id)
-						};
-					})
-					.sortBy(p => p.number || 99999 + p.name)
-					.map(p => {
-						const content = [];
-						if (includeNumbers) {
-							content.push(
-								<span
-									key="number"
-									className="number"
-									style={{ color: team.colours.trim1 }}
-								>
-									{p.number || ""}
-								</span>
-							);
-						}
-						content.push(
-							<span
-								key="name"
-								className="name"
-								style={{
-									color: p.isNew ? team.colours.trim1 : team.colours.text
-								}}
-							>
-								{p.name}
-							</span>
-						);
-
-						const props = { key: p.id, className: "player" };
-						if (team._id == localTeam && p.slug) {
-							return (
-								<Link {...props} to={`/players/${p.slug}`}>
-									{content}
-								</Link>
-							);
-						} else {
-							return <div {...props}>{content}</div>;
-						}
-					})
-					.value();
-
-				return (
-					<div
-						className="team-block"
-						key={team._id}
-						style={{
-							backgroundColor: team.colours.main,
-							textShadow: `0 0 2pt ${team.colours.main},-2px 2px 2pt ${team.colours.main}, 2px 2px 2pt ${team.colours.main},2px -2px 2pt ${team.colours.main},-2px -2px 2pt ${team.colours.main}`
-						}}
-					>
-						<TeamImage team={team} />
-						{squad}
-					</div>
-				);
-			})
-			.filter(_.identity)
-			.value();
-
-		if (content.length == 0) {
+		if (!teamsWithSquads.length) {
 			return null;
 		}
 
-		return <div className="team-blocks">{content}</div>;
+		const content = teamsWithSquads.map(team => {
+			//Get Team Squad Object
+			const squadObject = game.pregameSquads.find(({ _team }) => _team == team._id);
+
+			//Get Previous Games squad
+			let previousSquad = this.getSquadList(team._id, true);
+
+			//If at least one player has a squad number, we set this to true
+			let includeNumbers = false;
+
+			//Get Players
+			const squad = _.chain(squadObject.squad)
+				.map(p => {
+					const { _player, number } = _.find(
+						game.eligiblePlayers[team._id],
+						({ _player }) => _player._id == p
+					);
+
+					if (number) {
+						includeNumbers = true;
+					}
+
+					return {
+						id: _player._id,
+						name: `${_player.name.first} ${_player.name.last}`,
+						number,
+						slug: _player.slug,
+						isNew: previousSquad && !previousSquad.find(id => id == _player._id)
+					};
+				})
+				.sortBy(p => p.number || 99999 + p.name)
+				.map(p => {
+					const content = [];
+					if (includeNumbers) {
+						content.push(
+							<span
+								key="number"
+								className="number"
+								style={{ color: team.colours.trim1 }}
+							>
+								{p.number || ""}
+							</span>
+						);
+					}
+					content.push(
+						<span
+							key="name"
+							className="name"
+							style={{
+								color: p.isNew ? team.colours.trim1 : team.colours.text
+							}}
+						>
+							{p.name}
+						</span>
+					);
+
+					const props = { key: p.id, className: "player" };
+					if (team._id == localTeam && p.slug) {
+						return (
+							<Link {...props} to={`/players/${p.slug}`}>
+								{content}
+							</Link>
+						);
+					} else {
+						return <div {...props}>{content}</div>;
+					}
+				})
+				.value();
+
+			return (
+				<div
+					className={`team-block ${teamsWithSquads.length === 1 ? "single-block" : ""}`}
+					key={team._id}
+					style={{
+						backgroundColor: team.colours.main,
+						textShadow: `0 0 2pt ${team.colours.main},-2px 2px 2pt ${team.colours.main}, 2px 2px 2pt ${team.colours.main},2px -2px 2pt ${team.colours.main},-2px -2px 2pt ${team.colours.main}`
+					}}
+				>
+					<TeamImage team={team} />
+					{squad}
+				</div>
+			);
+		});
+
+		return (
+			<div className="team-blocks" key="team-blocks">
+				{content}
+			</div>
+		);
 	}
 
 	render() {
-		const { dateRange, pregameSquads } = this.state.game;
-		let content;
+		const { localTeam } = this.props;
+		const { game } = this.state;
+		const content = [];
+
+		//Bool to affect section classname
 		let squadsFound = false;
 
-		if (pregameSquads.length) {
-			content = this.renderSquads();
+		const teams = [localTeam, game._opposition._id];
+		const teamsHavePregameSquads = _.chain(teams)
+			.map(_team => {
+				return { _team, hasSquad: Boolean(this.getSquadList(_team)) };
+			})
+			.groupBy("hasSquad")
+			.mapValues(o => _.map(o, "_team"))
+			.value();
+
+		//If any teams are missing squads and we have a confirmed date,
+		//render a due date section
+		if (!game.dateRange && teamsHavePregameSquads.false) {
+			const singleTeam =
+				teamsHavePregameSquads.false.length == 1 ? teamsHavePregameSquads.false[0] : null;
+			content.push(this.renderDueDate(singleTeam));
+		}
+
+		//If any teams do have squads, add them after
+		if (teamsHavePregameSquads.true) {
+			content.push(this.renderSquads());
 			squadsFound = true;
 		}
 
-		if (!content && !dateRange) {
-			content = this.renderDueDate();
-		}
-
-		if (content) {
+		if (content.length) {
 			return (
 				<section className={`pregame-squads ${squadsFound ? "with-squads" : ""}`}>
 					{content}
