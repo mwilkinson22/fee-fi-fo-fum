57743c7aa9962ef5a60d9b1f49141bf8983aae60
Mark Wilkinson <markwilko22@gmail.com>
2020-10-30 14:04:55 +0000

    Added error boundary to video

--- client/components/EmbeddedVideo.js
+++ client/components/EmbeddedVideo.js
@@ -1,12 +1,15 @@
 import React from "react";
+import ErrorBoundary from "~/client/components/ErrorBoundary";
 
 function EmbeddedVideo({ autoPlay, muted, src }) {
 	return (
-		<div className="custom-block video-wrapper">
-			<video autoPlay={autoPlay} controls={!autoPlay} muted={muted} loop={true}>
-				<source src={src} />
-			</video>
-		</div>
+		<ErrorBoundary>
+			<div className="custom-block video-wrapper">
+				<video autoPlay={autoPlay} controls={!autoPlay} muted={muted} loop={true}>
+					<source src={src} />
+				</video>
+			</div>
+		</ErrorBoundary>
 	);
 }
 
